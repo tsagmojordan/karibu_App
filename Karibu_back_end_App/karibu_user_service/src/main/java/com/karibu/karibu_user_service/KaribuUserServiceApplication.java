559c1a41141a49db488c658dce6c9e5d301ea8c2
jordantsagmo <jordantsgamo@gmail.com>
2024-01-18 23:43:03 +0100

    initialisation of user service

---
+++ Karibu_back_end_App/karibu_user_service/src/main/java/com/karibu/karibu_user_service/KaribuUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.karibu.karibu_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaribuUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaribuUserServiceApplication.class, args);
+	}
+
+}
---
+++ Karibu_back_end_App/karibu_user_service/src/test/java/com/karibu/karibu_user_service/KaribuUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karibu.karibu_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaribuUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
