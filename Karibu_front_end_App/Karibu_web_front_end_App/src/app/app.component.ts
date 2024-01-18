51b1ea63a7f938427cbf9fdc4e21fb8dfdaf3ebe
jordantsagmo <jordantsgamo@gmail.com>
2024-01-18 23:07:25 +0100

    initial commit

---
+++ Karibu_front_end_App/Karibu_web_front_end_App/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Karibu_web_front_end_App';
+}
